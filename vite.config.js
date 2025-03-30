e9e11511fcc99960c21149bee815a4784fa32178
derimer <jderimer@gmail.com>
2025-03-30 10:23:44 +0200

    Modif package.json

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/portfolio_3d/', // Chemin de base pour GitHub Pages
   plugins: [react()],
 })
+
