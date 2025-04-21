ba3ad74092bc857b45122bc426af759e3a538472
derimer <jderimer@gmail.com>
2025-04-21 16:20:48 +0200

    modification mobile et tablette

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
+import viteCompression from 'vite-plugin-compression';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio_3d/', // Chemin de base pour GitHub Pages
-  plugins: [react()],
-})
-
+  base: '/portfolio_3d/',
+  plugins: [
+    react(),
+    viteCompression(),
+    visualizer({ open: true })
+  ],
+  build: {
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          threejs: ['three', 'three-stdlib'],
+          vendor: ['react', 'react-dom'],
+          lottie: ['lottie-web'] // Isole lottie dans son propre chunk
+        }
+      }
+    }
+  },
+  optimizeDeps: {
+    include: [
+      'three',
+      'three-stdlib',
+      'lottie-web' // Force l'inclusion dans le pré-bundling
+    ],
+    exclude: ['three-stdlib/libs/lottie.js'] // Exclut le fichier problématique
+  }
+});
