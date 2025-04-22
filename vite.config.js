3b91b46f9606c630e42fd5f704036a83fecd24a7
derimer <jderimer@gmail.com>
2025-04-22 18:35:22 +0200

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/', // Changez cela si votre projet est dans un sous-dossier
-})
